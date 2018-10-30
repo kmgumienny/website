1f0381b2c1042b764db7c5837350844374b7580a
Kamil Gumienny <kmgumienny@gmail.com>
2018-10-29 20:22:05 -0400

    Minor changes

--- app.js
+++ app.js
@@ -3,24 +3,31 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs = require('express-handlebars');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.engine( 'hbs', hbs( {
+    helpers:{},
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts/',
+    partialsDir: __dirname + '/views/partials/'
+} ) );
+
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(express.static('routes'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,6 +43,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.listen(process.env.PORT || 8000, ()=>{
+    console.log('Server is listening on port 8000');
 });
 
 module.exports = app;
