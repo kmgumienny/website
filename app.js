e0ca4c02c3fe871d3502cf01c0ac6e68e5f4ce88
kmgumienny <kmgumienny@gmail.com>
2018-10-31 02:18:59 -0400

    TODO: Resume and contact details

--- app.js
+++ app.js
@@ -29,10 +29,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use(express.static('routes'));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
 app.use(function(err, req, res, next) {
